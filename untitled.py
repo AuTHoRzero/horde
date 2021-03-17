07e8159ecd8fe2207a9d39a0c1d11ea36768958b
AuTHoRzero <74671718+AuTHoRzero@users.noreply.github.com>
2021-03-17 23:33:46 +0300

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,7 @@
+https://petrocol.ru/schedule/39-55?json=1&key=411f595c4c1467dec55ad9b125d12217
+?group = "39-55"
+&key = <411f595c4c1467dec55ad9b125d12217>
+&[course = "2"]
+&current_week = "denominator"
+current_week_url = "https:\/\/petrocol.ru\/schedule\/2521\/?week=denominator"
+next_week_url = "https:\/\/petrocol.ru\/schedule\/2521\/?week=numerator"
