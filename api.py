6ce1f234eb49c828f0ed807fc0bcae2a4376779a
AuTHoRzero <74671718+AuTHoRzero@users.noreply.github.com>
2021-03-18 12:42:20 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,9 @@
 import requests
+import json
 
-answer = requests.get('https://petrocol.ru/schedule/39-55?json=1&key=411f595c4c1467dec55ad9b125d12217').json()
-print (answer)
+def jprint(obj):
+	page = json.dumps(obj,sort_keys=True,indent=4)
+	print(page)
+
+response = requests.get('https://petrocol.ru/schedule/39-55?json=1&key=411f595c4c1467dec55ad9b125d12217')
+jprint(response.json())
