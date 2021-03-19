0a208fdcec8096cdb133cb6c371df861710af384
AuTHoRzero <74671718+AuTHoRzero@users.noreply.github.com>
2021-03-19 22:32:38 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,33 @@
+#coding=utf-8
 import requests
 import json
+import urllib.request
+import datetime
 
-def jprint(obj):
-	page = json.dumps(obj,sort_keys=True,indent=4)
-	print(page)
+zapros = 1
 
-response = requests.get('https://petrocol.ru/schedule/39-55?json=1&key=411f595c4c1467dec55ad9b125d12217')
-jprint(response.json())
+while zapros==1:
+	api_key ='411f595c4c1467dec55ad9b125d12217'
+	group = input()
+	url = 'https://petrocol.ru/schedule/'+group+'?json=1&key='+api_key
+	response = requests.get(url)
+	week=datetime.datetime.today().isoweekday()
+	if week == 1:
+		week="monday"
+	elif week == 2:
+		week='tuesday'
+	elif week == 3:
+		week='wednesday'
+	elif week == 4:
+		week='thursday'
+	elif week == 5:
+		week='friday'
+	elif week == 6:
+		week='saturday'
+	elif week == 7:
+		week='sunday'
+	else:
+		week='eror'
+	response_json = json.loads(response.text)
+	result = response_json["schedule"][week]
+	jprint(result)
