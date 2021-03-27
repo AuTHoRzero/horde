7e1f1cf0ae82d4d47f39f2ab2b2822aa3adc8814
Authorzero <Author@deidara.ru>
2021-03-27 21:54:18 +0300

    Create keyboard.py

---
+++ keyboard.py
@@ -0,0 +1,7 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+    
+student_button = KeyboardButton('Я студент')
+teacher_button = KeyboardButton('Я преподаватель')
+button_who = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(student_button).add(teacher_button)
