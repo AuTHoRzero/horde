4a9e07b45db0ec761e919722c46e4134c00fa3a8
Authorzero <Author@deidara.ru>
2021-03-27 23:04:53 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,1 +1,9 @@
 
+import pandas
+import encodings
+import json
+excel_data_df = pandas.read_excel('groupparse.xlsx', sheet_name='TDSheet')
+
+json_str = excel_data_df.to_json()
+str = json.dumps(json_str, ensure_ascii=False, indent=4)
+print('Excel Sheet to JSON:\n', json.loads(str))
