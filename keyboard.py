b83a864e81b31db6df964ff751968f37c4c00af0
Authorzero <Author@deidara.ru>
2021-03-28 19:02:03 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -5,3 +5,5 @@ from aiogram.types import ReplyKeyboardRemove, \
 student_button = KeyboardButton('Я студент')
 teacher_button = KeyboardButton('Я преподаватель')
 button_who = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(student_button).add(teacher_button)
+register_button = KeyboardButton('Регистрация')
+button_register = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(register_button)
