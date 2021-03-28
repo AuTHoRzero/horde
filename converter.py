df1a9675dda029c490100ddc5a72b7c5821208f6
Authorzero <Author@deidara.ru>
2021-03-28 19:01:24 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,9 +1,6 @@
-
+import pprint
 import pandas
-import encodings
 import json
-excel_data_df = pandas.read_excel('groupparse.xlsx', sheet_name='TDSheet')
 
-json_str = excel_data_df.to_json()
-str = json.dumps(json_str, ensure_ascii=False, indent=4)
-print('Excel Sheet to JSON:\n', json.loads(str))
+data = pandas.read_excel('groupparse.xlsx', sheet_name='TDSheet')
+pprint.pprint (json.loads(data.to_json()))
