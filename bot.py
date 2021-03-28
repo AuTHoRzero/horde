8b2aceb03958b10984f854d5dc64e837183e9aee
Authorzero <Author@deidara.ru>
2021-03-28 19:00:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,41 +10,41 @@ from config import bot_token
 from aiogram.utils.helper import Helper, HelperMode, ListItem
 import keyboard as keyboard
 import datetime
-import calendar
 from datetime import date
+import calendar
 #Bot object
 bot= Bot(token=bot_token)
 #Bot dispetcher
-#Funczija dlya raboti bota
 dp = Dispatcher(bot)
 #Buttons
-#datetime
-#data i vremya
+#Weekday and date
+segodnya_day = datetime.datetime.today().weekday()
+days_naming = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]
 today = date.today()
 calendar.day_name[today.weekday()]
 #users database
-#sozdanie basy dannih i stolbca user_id
 conn = sqlite3.connect('users_database.db')
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS users(user_id INTEGER)')
 #Function
-#privetstvie i zanesenie dannih v sql
 @dp.message_handler(commands='start')
 async def start(message : types.Message):
-    await message.answer('Добро пожаловать в petroshedulebot, мои создатели: \nАверин Андрей\nПрохоров Евгений\nБерозко Роман\n\nCтуденты группы 39-55', reply_markup=keyboard.button_who)
-    await message.answer(today)
-    try:
-        conn = sqlite3.connect('users_database.db')
-        cur = conn.cursor()
-        cur.execute(f'INSERT INTO users VALUES("{message.from_user.id}")')
-        conn.commit()
-    except Exception as e:
-        print(e)
-        conn = sqlite3.connect('users_database.db')
-        cur = conn.cursor()
-        cur.execute(f'INSERT INTO users VALUES("{message.from_user.id}")')
-        conn.commit()
-#Poluchit info o sebe iz sql tablitci (pozhje otcluchit)
+    await message.answer('Добро пожаловать в petroshedulebot, мои создатели: \nАверин Андрей\nПрохоров Евгений\nБерозко Роман\n\nCтуденты группы 39-55', reply_markup=keyboard.button_register)
+    await message.answer(f'Сегодня: {today}\n{days_naming[segodnya_day]}')
+    conn = sqlite3.connect('users_database.db')
+    cur = conn.cursor()
+    cur.execute(f'INSERT INTO users VALUES("{message.from_user.id}")')
+    conn.commit()
+
+@dp.message_handler(text=['Регистрация'])
+async def register(message: types.Message):
+    await bot.send_message(message.from_user.id,'Кто ты?', reply_markup=keyboard.button_who)
+@dp.message_handler(text=['Я студент'])
+async def student_register(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Напиши номер своей группы')
+@dp.message_handler(text=['Я преподаватель'])
+async def teacher_register(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Данная функция находится в разработке, пожалуйста попробуйте позднее')
 @dp.message_handler(commands=['profile'])
 async def get_profile(msg: types.Message):
     conn = sqlite3.connect('users_database.db')
@@ -52,7 +52,6 @@ async def get_profile(msg: types.Message):
     cur.execute(f'SELECT * FROM users WHERE user_id = "{msg.from_user.id}"')
     result = cur.fetchall()
     await bot.send_message(msg.from_user.id, f'ID = {list(result[0])[0]}')
-#Chtob pri vidache oshibki bot ne viletal
 if __name__=='__main__':
     executor.start_polling(dp, skip_updates=True)
     
