3dde5cda8835b310fbf89db925bc9cfcdefdc715
Authorzero <Author@deidara.ru>
2021-03-28 11:28:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,16 +15,20 @@ from datetime import date
 #Bot object
 bot= Bot(token=bot_token)
 #Bot dispetcher
+#Funczija dlya raboti bota
 dp = Dispatcher(bot)
 #Buttons
 #datetime
+#data i vremya
 today = date.today()
 calendar.day_name[today.weekday()]
 #users database
+#sozdanie basy dannih i stolbca user_id
 conn = sqlite3.connect('users_database.db')
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS users(user_id INTEGER)')
 #Function
+#privetstvie i zanesenie dannih v sql
 @dp.message_handler(commands='start')
 async def start(message : types.Message):
     await message.answer('Добро пожаловать в petroshedulebot, мои создатели: \nАверин Андрей\nПрохоров Евгений\nБерозко Роман\n\nCтуденты группы 39-55', reply_markup=keyboard.button_who)
@@ -40,6 +44,7 @@ async def start(message : types.Message):
         cur = conn.cursor()
         cur.execute(f'INSERT INTO users VALUES("{message.from_user.id}")')
         conn.commit()
+#Poluchit info o sebe iz sql tablitci (pozhje otcluchit)
 @dp.message_handler(commands=['profile'])
 async def get_profile(msg: types.Message):
     conn = sqlite3.connect('users_database.db')
@@ -47,6 +52,7 @@ async def get_profile(msg: types.Message):
     cur.execute(f'SELECT * FROM users WHERE user_id = "{msg.from_user.id}"')
     result = cur.fetchall()
     await bot.send_message(msg.from_user.id, f'ID = {list(result[0])[0]}')
+#Chtob pri vidache oshibki bot ne viletal
 if __name__=='__main__':
     executor.start_polling(dp, skip_updates=True)
     
