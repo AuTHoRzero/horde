3c4d47847e3820ab40ed3e6607c2e0b8f05edc98
Authorzero <Author@deidara.ru>
2021-04-05 09:02:06 +0000

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -17,4 +17,12 @@ schedule_button = KeyboardButton('Получить расписание')
 button_main = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(schedule_button).add(setting_button).add(profile_button).add(help_button)
 #Group 4
 go_to_main_button = KeyboardButton('Перейти в главное меню')
-buttn_go_main = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(go_to_main_button)
+button_go_main = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(go_to_main_button)
+#Group 5
+today_button = KeyboardButton('Расписание на сегодня')
+next_day_button = KeyboardButton('Расписание на завтра')
+button_schedule_choise = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(today_button).add(next_day_button)
+#Group 6
+time_set_button = KeyboardButton('Установить время, когда приходят уведомления')
+notify_switch_button = KeyboardButton('Включение/Выключение уведомлений')
+button_notify = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(time_set_button).add(notify_switch_button)
