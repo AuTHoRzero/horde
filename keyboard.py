36ffd93fdc02ce74b07e08de8af4f70d18992df8
Authorzero <Author@deidara.ru>
2021-04-06 20:51:25 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,6 +2,9 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
+#Button back
+back_btn = KeyboardButton('Назад')
+btn_back = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(back_btn)
 #Group 1    
 student_button = KeyboardButton('Я студент')
 teacher_button = KeyboardButton('Я преподаватель')
@@ -21,8 +24,8 @@ button_go_main = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=Tru
 #Group 5
 today_button = KeyboardButton('Расписание на сегодня')
 next_day_button = KeyboardButton('Расписание на завтра')
-button_schedule_choise = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(today_button).add(next_day_button)
+button_schedule_choise = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(today_button).add(next_day_button).add(back_btn)
 #Group 6
 time_set_button = KeyboardButton('Установить время, когда приходят уведомления')
 notify_switch_button = KeyboardButton('Включение/Выключение уведомлений')
-button_notify = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(time_set_button).add(notify_switch_button)
+button_notify = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(time_set_button).add(notify_switch_button).add(back_btn)
