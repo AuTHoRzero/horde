d826741cd3c6f883df76322ba80b70d0d15be6d7
Authorzero <Author@deidara.ru>
2021-04-08 18:12:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,12 @@ import keyboard as keyboard
 import datetime
 import calendar
 import lxml
-import aioschedule
+import requests
+import json
+import time
+import random
 import asyncio
+import aioschedule
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher, FSMContext
@@ -17,23 +21,30 @@ from aiogram.utils import executor
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.helper import Helper, HelperMode, ListItem
-from config import bot_token
+from config import bot_token, api_key
 from datetime import date
 from bs4 import BeautifulSoup
 from markdown import markdown
+#Logging
+logging.basicConfig(level=logging.INFO)
 #States
 class States(StatesGroup):
     group = State()
+    on_off = State()
+    main = State()
+    setting = State()
+    profile = State()
 #Bot object
 bot= Bot(token=bot_token)
 #Bot dispetcher
 dp = Dispatcher(bot, storage=MemoryStorage())
-#Buttons
 #Weekday and date
 today_day = datetime.datetime.today().weekday()
 days_naming = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]
+days_naming_en = ["monday", "tuesday", "wednesday", "thursday", "friday","saturday", "sunday"]
 today = date.today()
 calendar.day_name[today.weekday()]
+next_day = today_day + 1
 #users database
 conn = sqlite3.connect('users_database.db')
 cur = conn.cursor()
@@ -46,6 +57,7 @@ async def start(message : types.Message):
         reply_markup=keyboard.button_register
         )
     await message.answer(f'Сегодня: {today}\n{days_naming[today_day]}')
+    print (next_day)
 
 
 @dp.message_handler(text=['Регистрация'])
@@ -64,7 +76,12 @@ async def group_number(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         global group
         data['group'] = message.text
-        await bot.send_message(message.chat.id, md.text(md.text('Сышишь, другалёк, ты в группе', md.bold(data['group']))), parse_mode=ParseMode.MARKDOWN_V2)
+        await bot.send_message(message.chat.id, 
+        md.text(md.text('Ваша группа:', 
+        md.bold(data['group']))), 
+        parse_mode=ParseMode.MARKDOWN_V2,
+        reply_markup= keyboard.button_go_main
+        )
         grouper = md.text(md.text(md.bold(data['group'])))
         data = markdown(grouper)
         group = ''.join(BeautifulSoup(data).findAll(text=True))
@@ -73,8 +90,7 @@ async def group_number(message: types.Message, state: FSMContext):
         cur = conn.cursor()
         cur.execute(f'INSERT INTO users VALUES("{message.from_user.id}","{group}")')
         conn.commit()
-
-    await state.finish()
+        await state.finish()
 
 
 @dp.message_handler(text=['Я преподаватель'])
@@ -84,16 +100,134 @@ async def teacher_register(message: types.Message):
         'Данная функция находится в разработке, пожалуйста попробуйте позднее',
         )
 
+@dp.message_handler(text=['Настройки'])
+async def setting(message: types.Message):
+    await message.answer('Добро пожаловать в меню настроек', 
+    reply_markup=keyboard.button_notify,
+    )
 
-@dp.message_handler(commands=['profile'])
+
+@dp.message_handler(text=['Сменить группу'])
+async def rewrite (message: types.Message):
+    con = sqlite3.connect('users_database.db')
+    cur = con.cursor()
+    cur.execute(f'DELETE from users WHERE user_id = "{message.from_user.id}"')
+    con.commit()
+    cur.close()
+    await message.answer('Готово')
+    await States.group.set()
+    await message.answer('Введите новый номер группы')
+    
+
+
+@dp.message_handler(text=['Установить время, когда приходят уведомления'])
+async def time_quest (message: types.Message):
+    await message.answer('Функция в разработке')
+
+
+
+@dp.message_handler(text=['Включение/Выключение уведомлений'])
+async def time_set (message: types.Message):
+   await States.on_off.set()
+   await message.answer('Состояние успешно изменено, текущее состояние:\n')
+
+
+#@dp.message_handler(state=States.on_off)
+#async def on_off_change (message: types.Message):
+#    await message.answer('Hi')
+
+
+@dp.message_handler(text=['Получить расписание'])
+async def schedule_menu(message: types.Message):
+    await message.answer('Выберите на какой день получить расписание', 
+    reply_markup=keyboard.button_schedule_choise,
+    )
+
+
+@dp.message_handler(text=['Расписание на сегодня'])
+async def schedule_today(message: types.Message):
+    await message.answer(f'День недели: {days_naming[today_day]}')
+    con = sqlite3.connect('users_database.db')
+    cur = con.cursor()
+    cur.execute(f'SELECT * FROM users WHERE user_id = "{message.from_user.id}"')
+    res = cur.fetchall()
+    response = requests.get(f'https://petrocol.ru/schedule/{[list(res[0])[1]][0]}?json=1&key={api_key}')
+    all_schedule = json.loads(response.text)
+    try:
+        schedule = all_schedule["schedule"][days_naming_en[today_day]]
+    except:
+        await message.answer('Нет данных о парах на сегодня, попробуйте посмотреть на сайте:\nhttps://portal.petrocollege.ru/Pages/responsiveSh-aspx.aspx')
+    else:
+        for i in range(len(schedule)):
+            lesson_json = schedule[str(i+1)][0]
+            lesson = lesson_json['lesson']
+            teacher = lesson_json['teacher']
+            classroom = lesson_json['classroom']
+            period = f'Пара {i+1}:\n {lesson}, {teacher}, {classroom}\n'
+            reply_message = f'{period}'
+            await bot.send_message(message.from_user.id, reply_message)
+
+
+@dp.message_handler(text=['Расписание на завтра'])
+async def schedule_next_day(message: types.Message):
+    await message.answer(f'День недели: {days_naming[today_day]}')
+    con = sqlite3.connect('users_database.db')
+    cur = con.cursor()
+    cur.execute(f'SELECT * FROM users WHERE user_id = "{message.from_user.id}"')
+    res = cur.fetchall()
+    response = requests.get(f'https://petrocol.ru/schedule/{[list(res[0])[1]][0]}?json=1&key={api_key}')
+    all_schedule = json.loads(response.text)
+    try:
+        schedule = all_schedule["schedule"][days_naming_en[next_day]]
+    except:
+        await message.answer('Нет данных о парах на завтра, попробуйте посмотреть на сайте:\nhttps://portal.petrocollege.ru/Pages/responsiveSh-aspx.aspx')
+    else:
+        for i in range(len(schedule)):
+            try:
+                lesson_json = schedule[str(i+1)][0]
+                lesson = lesson_json['lesson']
+                teacher = lesson_json['teacher']
+                classroom = lesson_json['classroom']
+                period = f'Пара {i+1}:\n {lesson}, {teacher}, {classroom}\n'
+                reply_message = f'{period}'
+                await bot.send_message(message.from_user.id, reply_message)
+            except:
+                pass
+
+
+@dp.message_handler(text=['Перейти в главное меню', 'Вернутся в главное меню', 'Назад'])
+async def main_menu (message: types.Message):
+    await bot.send_message(message.from_user.id, 
+        'Добро пожаловать в главное меню, здесь вы можете настроить автоматическое получение получение расписания или получить его вручную', 
+        reply_markup=keyboard.button_main,
+        )
+
+
+@dp.message_handler(text=['Мой профиль'])
 async def get_profile(message: types.Message):
     conn = sqlite3.connect('users_database.db')
     cur = conn.cursor()
     cur.execute(f'SELECT * FROM users WHERE user_id = "{message.from_user.id}"')
     result = cur.fetchall()
-    await bot.send_message(message.from_user.id, f'ID = {list(result[0])[0]}\nGroup = {[list(result[0])[1]][0]}')
+    await bot.send_message(message.from_user.id, f'ID = {list(result[0])[0]}\nGroup = {[list(result[0])[1]][0]}', reply_markup=keyboard.btn_back)
+
+
+@dp.message_handler(text=['Помощь'])
+async def user_help (message: types.Message):
+    photo = ['https://sun9-45.userapi.com/impg/L_ZjDqZoxr0-Ps0fQi0-c48PjJ-UWJk64exZqw/HrcqjPtfIjE.jpg?size=840x737&quality=96&sign=e78ad3ba428e817729e80c0f02d249df&type=album', 
+    'https://sun9-30.userapi.com/impg/S-bSLtCaDlC1bcUwMCDlCAyzerrNVqFgw5Ygpg/BoLXpwQ1HcY.jpg?size=608x770&quality=96&sign=e101f67bcaa95f1aec2d52a651d24cef&type=album', 
+    'https://sun9-64.userapi.com/impg/v9TI88OR_8UV_CJ2u2FRJlSjFiRhpoh_lFKSFg/jPCHxw5V4WQ.jpg?size=1125x1077&quality=96&sign=b62f3c74fd48e7831d66add4eb792715&type=album',
+    ]
+    await message.answer('Не готово...', reply_markup = keyboard.btn_back)
+    await bot.send_photo(message.from_user.id, photo[random.randint(0,2)])
+
+async def scheduler():
+    await aioschedule.every().day.at("21:00").do(schedule_next_day)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
 
 
 if __name__=='__main__':
-    executor.start_polling(dp, skip_updates=False)
+    executor.start_polling(dp, skip_updates=True)
     
