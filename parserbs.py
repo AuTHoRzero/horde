bc010af829b02141a38c871d620ff6ffa1ed1248
Authorzero <Author@deidara.ru>
2021-04-18 23:08:18 +0300

    Update parserbs.py

--- parserbs.py
+++ parserbs.py
@@ -28,3 +28,17 @@ for rw in table.find_all("tbody"):
         cur.execute(f'INSERT OR REPLACE INTO raspis VALUES("{num_gr}","{num_les}","{less_do}","{less_af}")')
         conn.commit()
 
+
+table = soup.find("table", class_= 'ms-rteTable-default').find_next("table", class_= 'ms-rteTable-default')
+for rw in table.find_all("tbody"):
+    rows = rw.find_all('tr')
+    for row in rows:
+        first = row.find_all('td')
+        num_gr = first[0].get_text()
+        num_les = first[1].get_text()
+        less_do = first[2].get_text()
+        less_af = first[3].get_text()
+        print(f'{num_gr} {num_les} {less_do} {less_af}')
+        cur.execute(f'INSERT OR REPLACE INTO raspis VALUES("{num_gr}","{num_les}","{less_do}","{less_af}")')
+        conn.commit()
+
